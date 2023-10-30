50ef5d741414778b580f9156d1fdc7b561c9a807
zhangshuhao01 <zhangshuhao01@qianxin.com>
2023-10-30 17:49:28 +0800

    清除旧代码

--- Log/logger.py
+++
@@ -1,9 +0,0 @@
-import logging
-from Config.config import LOG_PATH
-logging.basicConfig(
-    level=logging.INFO,
-    filename=LOG_PATH,
-    datefmt='%Y/%m/%d %H:%M:%S',
-    format='%(asctime)s - %(levelname)s - %(process)d - %(module)s - %(funcName)s - %(lineno)d : %(message)s'
-)
-logger = logging.getLogger(__name__)
